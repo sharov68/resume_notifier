669ab9b398c36cc2d140d06669cadc13e8fff49b
Alexey Sharov <alexey.sharov@pushok.com>
2025-02-09 07:41:15 +0000

    clear app

--- bin/resume_notifier.dart
+++
@@ -1,5 +0,0 @@
-import 'package:resume_notifier/resume_notifier.dart' as resume_notifier;
-
-void main(List<String> arguments) {
-  print('Hello world: ${resume_notifier.calculate()}!');
-}
--- test/resume_notifier_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:resume_notifier/resume_notifier.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
